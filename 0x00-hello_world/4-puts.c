669734a1d16a95c15f67e4c872304e5d8a09aa89
Chiropractar <louisemersons@yahoo.com>
2022-09-08 21:07:32 -0700

    jand

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
 /**
  *  * main - Entry point
- *
- *    * Return: Always 0 (Success)
- *     */
+ *   *
+ *    * Return: always 0
+*/
 int main(void)
 {
-	    return (0);
+	puts("\" Programming is like building a multilingual puzzle");
+	return (0);
+}

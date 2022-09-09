7ad4bcb7b82198045ddb8802bc2318aee020a780
Chiropractar <louisemersons@yahoo.com>
2022-09-08 20:29:08 -0700

    wahala

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h> 
-/** 
- * * main - Entry point 
- *  * 
- *   * Return: always 0 
-*/ 
- int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle"); 
-				                           return (0)
-}
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	    return (0);

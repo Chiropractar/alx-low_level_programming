ecef04cef0403a3eb0f8140e1949f9a931d0f8ef
Chiropractar <louisemersons@yahoo.com>
2022-09-14 21:22:39 -0700

    shiiiii

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
-   * _isalpha - Shows 1 if the input is a
-        * letter Another cases, shows 0
-        * 
-	* @c: The character in ASCII code
-	*
-	* Return: 1 for leters. 0 for the rest.
-	*/
+   * _isalpha - Entry point
+   * @c: the integer value it receives
+   * Description: checks if alphabet
+   * Return: 1 if true. 0 if false
+   */
+
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
@@ -18,5 +21,4 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
-	_putchar('\n');
 }

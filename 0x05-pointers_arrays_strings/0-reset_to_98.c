669cff1da1531851e81de217b70836ef0ce3295e
Chiropractar <louisemersons@yahoo.com>
2022-09-20 11:09:49 -0700

    bliss

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset to 98
+ * update the value of the ariable ,the pointer points to 98
+ * @n: pointer to the variable to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

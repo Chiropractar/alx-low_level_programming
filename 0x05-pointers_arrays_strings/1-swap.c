039e57c31e00ceae0f0afd2577a62f5b703d2775
Chiropractar <louisemersons@yahoo.com>
2022-09-20 11:52:44 -0700

    abeg

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps two integers
+ * @2: pointer to first value
+ * @b: pointer to second value
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*B = c;
+}
